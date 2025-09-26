66c24fb6974844bcc8dd6eadb41c5bd7c7e2a3bb
Toddpaech <a1923435@adelaide.edu.au>
2025-09-26 16:30:17 +0930

    Update Cipher.hpp

--- Include/Cipher.hpp
+++ Include/Cipher.hpp
@@ -1,8 +1,14 @@
 #ifndef CIPHER_HPP
 #define CIPHER_HPP
+#include <string>
+
+using namespace std;
 
 class Cipher {
+    public:
+    static string encrypt(string plaintext, string key);
 
+    static string decrypt(string ciphertext, string key);
 };
 
 #endif
