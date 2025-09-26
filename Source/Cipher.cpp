fe40688b419bb6660abfb6d5cf1b70e581f50bb8
Toddpaech <a1923435@adelaide.edu.au>
2025-09-26 16:30:37 +0930

    Update Cipher.cpp

--- Source/Cipher.cpp
+++ Source/Cipher.cpp
@@ -1,1 +1,27 @@
 #include "../include/Cipher.hpp"
+
+string Cipher::encrypt(string plaintext, string key) {
+    string ciphertext = plaintext;
+    size_t keyLength = key.size();
+
+    for (size_t i = 0; i < plaintext.size(); i++) {
+        char p = (plaintext[i] - 'A');
+        char k = (key[i % keyLength] - 'A');
+        ciphertext[i] = (p + k) % 26 + 'A';
+    }
+
+    return ciphertext;
+}
+
+string Cipher::decrypt(string ciphertext, string key) {
+    string plaintext = ciphertext;
+    size_t keyLength = key.size();
+
+    for (size_t i = 0; i < ciphertext.size(); i++) {
+        char c = (ciphertext[i] - 'A');
+        char k = (key[i % keyLength] - 'A');
+        plaintext[i] = (c - k + 26) % 26 + 'A';
+    }
+
+    return plaintext;
+}
